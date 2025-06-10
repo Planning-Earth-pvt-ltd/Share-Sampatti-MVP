ad1881bf77a2723317fe50742360c965efa9d803
Phool Prakash <phool.prakash@newtabtechnologies.com>
2025-06-10 15:45:27 +0530

    AppConstants

---
+++ lib/core/config/app_constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  //about app
+  static const String appName = "Share Sampatti";
+  static const double appVersion = 1.0;
+  static const String aboutApp = "Share your Sampatti";
+  static const String supportEmail = "";
+  static const String supportPhone = "";
+
+  //fonts
+
+ static const String interFontFamily = "Inter";
+ static const String spaceGrotesk = "Space Grotesk";
+}
