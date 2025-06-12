4d1dd948a905902d70fe5ae5af666d6bc778dd7d
Phool Prakash <phool.prakash@newtabtechnologies.com>
2025-06-12 18:06:12 +0530

    Splash Screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,6 @@
 export 'package:share_sampatti_mvp/common/common.dart';
 export 'package:share_sampatti_mvp/scr/src.dart';
 export 'package:share_sampatti_mvp/core/core.dart';
+
+//app
+export 'package:share_sampatti_mvp/app/splash_screen/splash_screen.dart';
--- lib/scr/src.dart
+++ lib/scr/src.dart
@@ -1,3 +1,4 @@
 export 'package:share_sampatti_mvp/scr/features/auth/auth.dart';
 export 'package:share_sampatti_mvp/scr/features/onboarding/onboarding.dart';
 export 'package:share_sampatti_mvp/scr/features/home/home.dart';
+
