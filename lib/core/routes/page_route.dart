cd2fcf48c429a3d2b66903c9706319254c0682ea
Phool Prakash <phool.prakash@newtabtechnologies.com>
2025-06-12 18:08:31 +0530

    Splash Screen

--- lib/core/routes/page_route.dart
+++ lib/core/routes/page_route.dart
@@ -1,11 +1,6 @@
 import 'package:go_router/go_router.dart';
-<<<<<<< Updated upstream
-import 'package:share_sampatti_mvp/scr/features/auth/screens/otp_screen.dart';
-
 import 'package:share_sampatti_mvp/scr/src.dart';
-=======
-import 'package:share_sampatti_mvp/app/app.dart';
->>>>>>> Stashed changes
+import '../../app/splash_screen/splash_screen.dart';
 
 final route = GoRouter(
   routes: [
@@ -13,7 +8,6 @@ final route = GoRouter(
     GoRoute(path: '/onboarding', builder: (context, state) => Onboarding()),
     GoRoute(path: '/login', builder: (context, state) => Login()),
     GoRoute(path: '/signUp', builder: (context, state) => SignUp()),
-    GoRoute(path: '/otpScreen', builder: (context, state) => OtpScreen()),
     GoRoute(path: '/home', builder: (context, state) => Home()),
   ],
 );
